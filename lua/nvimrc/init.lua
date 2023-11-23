c6fb192fbd21fcc69e8a09d423c7d599519f3a8c
012e <huyphmnhat@gmail.com>
2023-11-23 12:49:18 +0700

    debug

--- lua/nvimrc/init.lua
+++ lua/nvimrc/init.lua
@@ -22,10 +22,10 @@ function M.setup(config)
 	local lua_file = config.pattern .. ".lua"
 	local vim_file = config.pattern
 
-	-- Get exit status of git command
-	vim.fn.system({ "git", "rev-parse", "--is-inside-work-tree" })
-	if vim.v.shell_error == 0 then
-		local git_root = vim.system({ "git", "rev-parse", "--show-toplevel" }):wait().stdout
+	-- Get exit code of git command
+	local code = vim.system({ "git", "rev-parse", "--is-inside-work-tree" }):wait().code
+	if code == 0 then
+		local git_root = vim.system({ "git", "rev-parse", "--show-toplevel" }, {text = true}):wait().stdout
 		print("path: " .. git_root .. utils.path_separator .. vim_file)
 		source_if_exists(git_root .. utils.path_separator .. vim_file)
 		luafile_if_exists(git_root .. utils.path_separator .. lua_file)
