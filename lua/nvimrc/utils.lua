9af2ec22b6ce3ddf5cbd1c6cf213fc8fe214b112
012e <huyphmnhat@gmail.com>
2023-11-23 13:02:37 +0700

    init

---
+++ lua/nvimrc/utils.lua
@@ -0,0 +1,20 @@
+local M = {}
+
+-- checks if a file exists
+function M.exists(path)
+	local f = io.open(path, "r")
+	if f ~= nil then
+		io.close(f)
+		return true
+	else
+		return false
+	end
+end
+
+M.path_separator = "/"
+M.is_windows = vim.fn.has("win32") == 1 or vim.fn.has("win32unix") == 1
+if M.is_windows == true then
+  M.path_separator = "\\"
+end
+
+return M
