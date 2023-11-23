505d26e8157c12ef8c165c801a754e1735ee9cfe
012e <huyphmnhat@gmail.com>
2023-11-23 12:52:47 +0700

    debug

--- lua/nvimrc/init.lua
+++ lua/nvimrc/init.lua
@@ -28,6 +28,7 @@ function M.setup(config)
 		local git_root = vim.system({ "git", "rev-parse", "--show-toplevel" }, { text = true }):wait().stdout
     print("root: "..git_root)
 		print("path: " .. git_root .. utils.path_separator .. vim_file)
+    print(config.pattern)
 		source_if_exists(git_root .. utils.path_separator .. vim_file)
 		luafile_if_exists(git_root .. utils.path_separator .. lua_file)
 	else
